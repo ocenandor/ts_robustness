7cee6ca6d1d636d35bf7b98ea99dee124a3a78bd
pavel.tikhomirov <pavel.tikhomirov@skoltech.ru>
2024-03-19 21:05:44 +0000

    create table with pert normes in the end

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 import json
 import sys
 import warnings
-warnings.filterwarnings('ignore')
-from tabulate import tabulate
 
+import joblib
+
+warnings.filterwarnings('ignore')
 from glob import glob
+
 import pandas as pd
+from tabulate import tabulate
+from tqdm import tqdm
 
 sys.path.append('.')
-from metrics import calcul_mean_confidence_interval
-
-
-from tools.train import train, MODELS
-from tools.attack import test, ATTACKS
-
-def form_cell(m, low, high, *args, conf_level=95):
-    return f'{m:.1f} {conf_level}% CI [{low:.1f}, {high:.1f}]'
+from metrics import create_table
+from src.utils import open_config
+from tools.attack import ATTACKS, test
+from tools.train import train
 
 if __name__ == '__main__':
-    configs = glob('configs/*.json')
-    results = pd.DataFrame(columns=MODELS.keys(), index=ATTACKS.keys())
 
+    ATTACK_PARMS = {'bim': (0.002, 200),
+                    'deepfool': (0.5, 50),
+                    'simba': (10, 1000)}
+
+    configs = glob('configs/*500.json')
+    results = {}
     for conf_file in configs: #TODO make with parse args to choose models and attacks
-        with open(conf_file) as f:
-            config = json.load(f)
+        config = open_config(conf_file)
         model_name = config['model']['name'] #TODO Think about logging, to make it more user-friendly
-        print('Process', model_name.upper())
-        train(config=config, wandb_log=False, save_dir='demo/', dataset_dir='data/FordA')
-        for attack in ATTACKS:
-            iters = test(config=config, weights=f'demo/{model_name}.pt', attack=attack, verbose=False)
-            cell = form_cell(iters.mean(), *calcul_mean_confidence_interval(iters))
-            results.loc[attack, model_name] = cell
+        print('Train', model_name.upper())
+        train(config=config, wandb_log=False, save_dir='demo/', dataset_dir='data/FordA', verbose=False)
+        for attack in tqdm(ATTACKS): 
+            results.setdefault(attack, {})   
+            print('Attack with', attack)  
+            iters, _, pert_norms = test(config, f'demo/{model_name}.pt', attack, *ATTACK_PARMS[attack], verbose=False)      
+            results[attack][model_name] = {'iters': iters, 'pert_norms': pert_norms}
+    
     print('\n                      ==========================')
-    print('                                DONE!           ')
+    print('                                DONE!')
     print('                      ==========================')
-    print("Mean number of iterations to change model's decision")
-    print(tabulate(results, headers='keys', tablefmt='psql'))
-    results.to_csv('results.csv')      
+    print("Mean robustness models over FordA dataset")
+    table = create_table(results)
+    print(tabulate(table, headers='keys', tablefmt='psql'))
+    table.to_csv('demo/results.csv')      
     print('results saved in results.csv')      
+    joblib.dump(results, 'demo/500_results.pkl')
